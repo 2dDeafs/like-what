5c7a2bd9bbeaba8169cb8d00309b4a09af869268
Jessica Reinheimer de Lima <jessica.lima@acad.pucrs.br>
2014-02-10 16:01:52 -0200

    mimimimimi

---
+++ Recorder2/SaveAudioViewController.h
@@ -0,0 +1,14 @@
+//
+//  SaveAudioViewController.h
+//  Recorder2
+//
+//  Created by Jessica Reinheimer de Lima on 2/10/14.
+//  Copyright (c) 2014 Matheus Cardoso. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SaveAudioViewController : UIViewController
+@property (weak, nonatomic) IBOutlet UITableView *tbAudio;
+
+@end
---
+++ Recorder2/SaveAudioViewController.m
@@ -0,0 +1,38 @@
+//
+//  SaveAudioViewController.m
+//  Recorder2
+//
+//  Created by Jessica Reinheimer de Lima on 2/10/14.
+//  Copyright (c) 2014 Matheus Cardoso. All rights reserved.
+//
+
+#import "SaveAudioViewController.h"
+
+@interface SaveAudioViewController ()
+
+@end
+
+@implementation SaveAudioViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
